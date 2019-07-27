b81da8666380fa8525d3dc1e5eb496503ce2d2e5
Brennan <brennanbuitendag@gmail.com>
2019-07-27 10:21:59 +0200

    - chore: Added pokemon mock data.  - feat: Added more pokemon types.

--- src/app/shared/abstractions/types/pokemon.type.ts
+++ src/app/shared/abstractions/types/pokemon.type.ts
@@ -1,2 +1,20 @@
-// TODO: Need more
-export type PokemonType = 'grass' | 'poison';
+export type PokemonType =
+  'normal' |
+  'fire' |
+  'fighting' |
+  'water' |
+  'flying' |
+  'grass' |
+  'poison' |
+  'electric' |
+  'ground' |
+  'psychic' |
+  'rock' |
+  'ice' |
+  'bug' |
+  'dragon' |
+  'ghost' |
+  'dark' |
+  'steel' |
+  'fairy';
+
